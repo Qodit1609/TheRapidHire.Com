3761ae8eee8dfe75431993c18c81135cb3e0b37e
khushbu133 <khushi74153@gmail.com>
2021-10-11 11:27:45 +0530

    new changes

--- src/Components/Apply/Apply.js
+++ src/Components/Apply/Apply.js
@@ -4,26 +4,30 @@ import Footer from "../Footer/Footer";
 import { useHistory } from "react-router-dom";
 import "./Apply.css";
 
- const Apply = () => {
-  
-    const [name, setName]=useState("")
-    const [email, setEmail]=useState("")
-    const [phone_number, setMobile]=useState("")
-    const [technology, setTechnology]=useState("")
-    const [description, setDescription]=useState("")
-    const [upload, setFile]=useState("")
-    const history=useHistory();
-    
- 
- async function applynow()
-  {
-    console.warn(name,email,phone_number,technology,description,upload)
-    const formData ={name,email,phone_number,technology,description,upload}
-    let result= await fetch ("https://qoditdev.herokuapp.com/careerform",{
-      method:'POST',
-      body:formData
-  });
-  alert("data has been saved")
+const Apply = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_number, setMobile] = useState("");
+  const [technology, setTechnology] = useState("");
+  const [description, setDescription] = useState("");
+  const [upload, setFile] = useState("");
+  const history = useHistory();
+
+  async function applynow() {
+    console.warn(name, email, phone_number, technology, description, upload);
+    const formData = {
+      name,
+      email,
+      phone_number,
+      technology,
+      description,
+      upload,
+    };
+    let result = await fetch("https://qoditdev.herokuapp.com/careerform", {
+      method: "POST",
+      body: formData,
+    });
+    alert("data has been saved");
   }
 
   return (
@@ -45,42 +49,116 @@ import "./Apply.css";
         </div>
       </div>
       <section class="apply-sec text-center login-block">
-      <div class="container d-flex justify-content-center">
-    <div class="form">
-        <div class="contact-info">
-            <h3 class="title">Apply Now</h3>
-            <p class="text-center1"> Apply latest IT-Software Jobs across India via TheRapidHire.com. Explore the latest Software Jobs in Indore and also job openings in your nearby locations. </p>
-           
-            
-        </div>
-        <div class="contact-info-form"> <span class="circle one"></span> <span class="circle two"></span>
-        <form class="sign-up">
+        <div class="container d-flex justify-content-center">
+          <div class="form">
+            <div class="contact-info">
+            <h3 id="summernote" class="listHeading">
+               Java Developer
+              </h3>
+              <ul class="listForJob">
+                <ul>
+                  <li>
+                    Experience in core Java advanced programming. Experience in
+                    Spring & Hibernate is mandatory. Experience in building
+                    mechanisms like Maven. Good knowledge on design patterns and
+                    anti-patterns. Good in algorithms and data structures.
+                    <li>
+                      {" "}
+                      Good understanding of Database concepts and Experience in
+                      SQL, PLSQL and NoSQL. Excellent Analytical and program
+                      solving skills Excellent written and oral communication
+                      skills self-starter and highly motivated Work in a dynamic
+                      environment and ability to adapt quickly to changes.</li><li>
+                      Experience with Test and defect management tools. An
+                      innovator, always suggesting newer ways of improving
+                      Productivity. Mentor and train team members. Identify and
+                      escalate Risks on time and suggest mitigation measures.
+                    </li>
+                  </li>
+                </ul>
+              </ul>
+            </div>
+            <div class="contact-info-form">
+              {" "}
+              <span class="circle one"></span> <span class="circle two"></span>
+              <form class="sign-up">
                 <h2 class="heading mb-4">Apply Now</h2>
-                <div class="form-group fone mt-2"> 
-                 <input type="name" value={name} onChange={(e)=>setName(e.target.value)} class="form-control" placeholder="Name"/> </div>
-                <div class="form-group fone mt-2"> 
-                <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} class="form-control" placeholder="Email Id"/> </div>
-                <div class="form-group fone mt-2"> 
-                 <input type="mobile_no" value={phone_number} onChange={(e)=>setMobile(e.target.value)} class="form-control" placeholder="Mobile Number"/></div>
-                <div class="form-group fone mt-2"> 
-                 <input type="text" value={technology} onChange={(e)=>setTechnology(e.target.value)} class="form-control" placeholder="Technology"/></div>
-                <div class="form-group fone mt-2"> 
-                 <textarea name="message" value={description} onChange={(e)=>setDescription(e.target.value)} class="form-control" placeholder=" Description"/></div>
-                <div class="form-group fone mt-2"> 
-                 <input type="file"  onChange={(e)=>setFile(e.target.files[0])} class="form-control" placeholder="upload file"/></div>
-                
-                
-              <input type="checkbox" class="form-check-input ml-0" id="exampleCheck1"/> <label class="form-check-label ml-3" for="exampleCheck1">I agree to Stoke <u>Terms</u> and <u>Privacy Policy</u></label>
-            </form> <button type="button" onClick={applynow} class="btn btn-success mb-5">Apply now</button>
-           
+                <div class="form-group fone mt-2">
+                  <input
+                    type="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    class="form-control"
+                    placeholder="Name"
+                  />{" "}
+                </div>
+                <div class="form-group fone mt-2">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    class="form-control"
+                    placeholder="Email Id"
+                  />{" "}
+                </div>
+                <div class="form-group fone mt-2">
+                  <input
+                    type="mobile_no"
+                    value={phone_number}
+                    onChange={(e) => setMobile(e.target.value)}
+                    class="form-control"
+                    placeholder="Mobile Number"
+                  />
+                </div>
+                <div class="form-group fone mt-2">
+                  <input
+                    type="text"
+                    value={technology}
+                    onChange={(e) => setTechnology(e.target.value)}
+                    class="form-control"
+                    placeholder="Technology"
+                  />
+                </div>
+                <div class="form-group fone mt-2">
+                  <textarea
+                    name="message"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    class="form-control"
+                    placeholder=" Description"
+                  />
+                </div>
+                <div class="form-group fone mt-2">
+                  <input
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    class="form-control"
+                    placeholder="upload file"
+                  />
+                </div>
+                <input
+                  type="checkbox"
+                  class="form-check-input ml-0"
+                  id="exampleCheck1"
+                />{" "}
+                <label class="form-check-label ml-3" for="exampleCheck1">
+                  I agree to Stoke <u>Terms</u> and <u>Privacy Policy</u>
+                </label>
+              </form>{" "}
+              <button
+                type="button"
+                onClick={applynow}
+                class="btn btn-success mb-5"
+              >
+                Apply now
+              </button>
+            </div>
+          </div>
         </div>
-    </div>
-</div>
-</section>
+      </section>
       <Footer />
     </div>
   );
 };
 
 export default Apply;
-
--- src/Components/Career/Career.js
+++ src/Components/Career/Career.js
@@ -13,10 +13,13 @@ import $ from "jquery";
 
 
 const Career = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   
-  const redirect = () => {
-    window.location.href = '/apply'
- }
+//   const redirect = () => {
+//     window.location.href = '/apply'
+//  }
   
   return (
     <div id="career">
@@ -106,6 +109,7 @@ const Career = () => {
                     Lorem Ipsum is simply dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1700s specimen book.
+                    
                   </p>
                 </div>
               </div>
@@ -200,9 +204,9 @@ const Career = () => {
             <div className="row pt-4">
               <div
                 className="col-md-4"
-                data-bs-toggle="modal"
-                data-bs-target="#java"
+                
               >
+                <Link to="/apply">
                 <div className="hire">
                   <div class="vacancy-box">
                     <h4>Java Developer</h4>
@@ -224,12 +228,14 @@ const Career = () => {
                     />
                   </div>
                 </div>
+              </Link>
               </div>
               <div
                 className="col-md-4"
                 data-bs-toggle="modal"
-                data-bs-target="#mern"
+              
               >
+                 <Link to="/apply">
                 <div className="hire">
                   <div class="vacancy-box">
                     <h4>Mern Developer</h4>
@@ -246,17 +252,19 @@ const Career = () => {
                     <img
                       class="forward-arrow"
                       src={Arrow}
-                      alt=""
+                     alt=""
                       data-backdrop="false"
                     />
                   </div>
                 </div>
+                </Link>
               </div>
               <div
                 className="col-md-4"
                 data-bs-toggle="modal"
-                data-bs-target="#mean"
+                
               >
+                 <Link to="/apply">
                 <div className="hire">
                   <div class="vacancy-box">
                     <h4>Mean Developer</h4>
@@ -278,276 +286,13 @@ const Career = () => {
                     />
                   </div>
                 </div>
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <div class="modal" id="java" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header align-items-start">
-              <div>
-                <div class="d-flex">
-                  <h5 class="modal-title">Java Developer</h5>
-                  <span>
-                    <img class="location-img" src={Location} alt="location" />
-                    Indore, India
-                  </span>
-                </div>
-                <p>Experience : 3+ Years</p>
-              </div>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body">
-              <h3 id="summernote" class="listHeading">
-                Job Description
-              </h3>
-              <ul class="listForJob">
-                <ul>
-                  <li>
-                    Experience in core Java advanced programming. Experience in
-                    Spring & Hibernate is mandatory. Experience in building
-                    mechanisms like Maven. Good knowledge on design patterns and
-                    anti-patterns. Good in algorithms and data structures.
-                    <li>
-                      {" "}
-                      Good understanding of Database concepts and Experience in
-                      SQL, PLSQL and NoSQL. Excellent Analytical and program
-                      solving skills Excellent written and oral communication
-                      skills self-starter and highly motivated Work in a dynamic
-                      environment and ability to adapt quickly to changes
-                      Experience with Test and defect management tools. An
-                      innovator, always suggesting newer ways of improving
-                      Productivity. Mentor and train team members. Identify and
-                      escalate Risks on time and suggest mitigation measures.
-                    </li>
-                  </li>
-                </ul>
-              </ul>
-              <h3 class="listHeading">Required Job Skills</h3>
-              <ul class="listForJob">
-                <ul>
-                  <li>
-                    Experience: 2-3+ years Salary: No bar for deserving
-                    candidates Location: Indore (work for office)
-                  </li>
-                  <li>
-                    Excellent written and oral communication skills Self-starter
-                    and highly motivated
-                    <li>
-                      {" "}
-                      Work in a dynamic environment and ability to adapt quickly
-                      with changes Ability to constantly innovate and think out
-                      of the box Good to have: Experience in the BFSI domain is
-                      an advantage.
-                    </li>{" "}
-                    Experience in AWS Cloud Experience in Node js/Vue js/Angular
-                    js. Knowledge on other J2ee frameworks • Hands-on experience
-                    using any Continuous Integration tools like Jenkins. • Hands
-                    on knowledge of JUNIT and code coverage, White-box testing •
-                    Exposure to CI/CD
-                    <li>
-                      Industry certification on technologies with expertise, or
-                      willingness to obtain certification. Experience with
-                      Firebase is plus; Strong knowledge of Object Oriented
-                      programming and design.
-                    </li>
-                    patterns.
-                  </li>
-                </ul>
-              </ul>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="submit"
-                onClick={redirect}
-                class="btn black-btn text-uppercase filled-btn"
-              >
-                APPLY NOW 
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal" id="mern" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header align-items-start">
-              <div>
-                <div class="d-flex">
-                  <h5 class="modal-title">Mern Developer</h5>
-                  <span>
-                    <img class="location-img" src={Location} alt="location" />
-                    Indore, India
-                  </span>
-                </div>
-                <p>Experience : 3+ Years</p>
-              </div>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body">
-              <h3 id="summernote" class="listHeading">
-                Job Description
-              </h3>
-              <ul class="listForJob">
-                <ul>
-                  <li>
-                    We are looking for a MERN Stack Developer to produce
-                    scalable software solutions. You’ll be part of a
-                    cross-functional team that’s responsible for the full
-                    software development life cycle, from conception to
-                    deployment.
-                  </li>
-                  <li>
-                    As a Full Stack Developer, you should be comfortable around
-                    both front-end and back-end coding languages, development
-                    frameworks and third-party libraries. You should also be a
-                    team player with a knack for visual design and utility. If
-                    you’re also familiar with Agile methodologies, we’d like to
-                    meet you.
-                  </li>
-                  <li>Write maintainable scalable efficient code</li>
-                </ul>
-              </ul>
-              <h3 class="listHeading">Required Job Skills</h3>
-              <ul class="listForJob">
-                <ul>
-                  <li>
-                    Experience: 3+ years Salary: No bar for deserving candidate
-                    Location: Indore (Work for office)
-                  </li>
-                  <li>
-                    Engineering degree in Computer Science related fields, or
-                    M.C.A. or B.Tech; Experience with Express.js;
-                    <li>
-                      {" "}
-                      Experience with react.js; Passion for best design and
-                      coding practices and a desire to develop new bold ideas;
-                    </li>{" "}
-                    Experience with Node.js; Experience with MongoDB; Experience
-                    with other stacks such as PHP is plus, but not mandatory;{" "}
-                    <li>
-                      Industry certification on technologies with expertise, or
-                      willingness to obtain certification. Experience with
-                      Firebase is plus; Strong knowledge of Object Oriented
-                      programming and design.
-                    </li>
-                    patterns.
-                  </li>
-                </ul>
-              </ul>
-            </div>
-            <div class="modal-footer">
-             <button
-                type="submit"
-                onClick={redirect}
-                class="btn black-btn text-uppercase filled-btn"
-              >
-                APPLY NOW 
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal" id="mean" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header align-items-start">
-              <div>
-                <div class="d-flex">
-                  <h5 class="modal-title">Mean Developer</h5>
-                  <span>
-                    <img class="location-img" src={Location} alt="location" />
-                    Indore, India
-                  </span>
-                </div>
-                <p>Experience : 3+ Years</p>
-              </div>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body">
-              <h3 id="summernote" class="listHeading">
-                Job Description
-              </h3>
-              <ul class="listForJob">
-                <ul>
-                  <li>
-                    Our company is looking for a Mean Stack Developer to join
-                    our team.
-                  </li>
-                  <li>
-                    Proven interpersonal skills while contributing to team
-                    effort by accomplishing related results as needed; Scope and
-                    deliver solutions with the ability to design solutions
-                    independently based on high-level architecture;
-                  </li>
-                  <li>
-                    Independent thinking, Desire / Ability to work in a
-                    fast-paced environment with creativity and brainstorming;
-                    Work on the new features that our Push Notification tool
-                    demands;
-                  </li>
-                </ul>
-              </ul>
-              <h3 class="listHeading">Required Job Skills</h3>
-              <ul class="listForJob">
-                <ul>
-                  <li>
-                    Experience: 3+ years Salary: No bar for deserving candidates
-                    Location: Indore (Work for office)
-                  </li>
-                  <li>
-                    Expertise in developing applications with Angular
-                    (specifically Angular 6) and using Bootstrap for responsive
-                    web design; Manage the mean-stack for an existing SaaS
-                    software;
-                  </li>
-                  <li>
-                    Expert in MongoDB and familiarity with RDBMS databases
-                    (mySQL, etc.); Previous experience building full-stack web
-                    applications with NodeJS / Express and integrating third
-                    party APIs (Google, Facebook, Twilio, etc.); Work with
-                    senior executives on everything from bug fixes to new
-                    feature deployment;
-                  </li>
-                  <li>
-                    Ability to work independently or with a group; Strong
-                    problem- solving, written, and verbal communication skills;
-                    Experience writing node.js + Express/Sails + PostgreSQL (or
-                    mySQL) code in production;
-                  </li>
-                </ul>
-              </ul>
-            </div>
-            <div class="modal-footer">
-             <button
-                type="submit"
-                onClick={redirect}
-                class="btn black-btn text-uppercase filled-btn"
-              >
-                APPLY NOW 
-              </button>
-            </div>
-          </div>
-          
-        </div>
-      </div>
+      
       <Footer />
     </div>
   );
